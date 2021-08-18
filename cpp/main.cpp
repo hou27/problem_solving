07c4b87a6c925361e593da773f6c55567804f837
김정호 <starjhjh@naver.com>
2021-08-18 14:58:47 +0000

    commit 0818

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,17 +1,44 @@
-#include <iostream>
 #include <string>
 #include <vector>
-
-// extern int solution();
+#include <stack>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-  double test;
-
-  cin >> test;
-  cout << "Hello World!\n" << test << endl;
-  // solution();
+void showAll(stack<int> s) {
+    while (!s.empty()) {
+        cout << s.top() << ' ';
+        s.pop();
+    }
 }
 
-int main();
+int solution(vector<vector<int>> board, vector<int> moves) {
+    
+    int answer = 0;
+    stack<int> basket;
+    
+    for(int l = 0; l < moves.size(); l++) {
+        int location = moves[l] - 1;
+        for(int i = 0; i < board.size(); i++) {
+            
+            if(board[i][location] != 0) {
+                cout << board[i][location] << endl;
+                if(!basket.empty() && basket.top() == board[i][location]) {
+                    cout << "stack ";
+                    showAll(basket);
+                    basket.pop();
+                    answer += 2;
+                    continue;
+                }
+                else {
+                    cout << "input "<<board[i][location] << endl;
+                    basket.push(board[i][location]);
+                }
+                    
+                board[i][location] = 0;
+            }
+        }
+    }
+	
+    return answer;
+}
