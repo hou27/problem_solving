e5d6ed0d2a686c38bf0a94ad6be9c00ea5e55e83
김정호 <starjhjh@naver.com>
2021-08-27 14:06:05 +0000

    commit kakaoQ2__add comment

--- cpp/KakaoWinter/KakaoQ2.cpp
+++ cpp/KakaoWinter/KakaoQ2.cpp
@@ -31,10 +31,10 @@ vector<int> solution(string s) {
         }
     }
     
-    sort(pairV.begin(), pairV.end());
-    reverse(pairV.begin(), pairV.end());
+    sort(pairV.begin(), pairV.end());	// sort함수는 algorithm헤더파일을 삽입 후 사용 가능. 기본적으로 pair의 왼쪽 값으로 정렬되며, 3번째 인자로 사용자 정의 함수를 넘겨주어 다른 방식으로 정렬 또한 가능하다.
+    reverse(pairV.begin(), pairV.end());	// reverse함수는 algorithm헤더파일을 삽입 후 사용 가능.
     
-    for(auto element : pairV){
+    for(auto element : pairV){	// auto
         answer.push_back(element.second);
     }
     
