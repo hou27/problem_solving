ccd77664d9a9cec3d2aff18d3a084505c49ce56d
hou27 <ataj125@gmail.com>
2022-02-24 18:05:03 +0900

    Finally solved 1033

--- c/baekjoon/1033_wrong.c
+++ c/baekjoon/1033_wrong.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
+#include<malloc.h>
 
 // 1033 칵테일
+// 아직 못 품
 
 typedef struct material {
 	int line;
 	int val;
 }mtr;
 
-long GCD(long a, long b);
-// int LCM(int a, int b);
+long GCD(int a, int b);
+int LCM(int a, int b);
 void update(int idx, int val, int line, int cnt);
 
+
 int N;
+mtr material[10];
 
-mtr material[15];
-
-int main() {
+void main() {
 	scanf("%d", &N);
 
 	for (int i = 0; i < N; i++) {
 		material[i].val = 1;
-		material[i].line = -1;
 	}
 
 	for (int i = 0; i < N - 1; i++) {
@@ -29,30 +30,20 @@ int main() {
 		int aVal, bVal;
 
 		scanf("%d %d %d %d", &a, &b, &p, &q);
+		material[a].line = i;
+		material[b].line = i;
 		gcd = (int)GCD(p, q);
 		aVal = material[b].val * p / gcd;
 		bVal = material[a].val * q / gcd;
 		gcd = (int)GCD(aVal, bVal);
 		update(a, aVal / gcd, i, 1);
 		update(b, bVal / gcd, i, 1);
-
-		// printf("middle chk\n");
-		// for (int i = 0; i < N; i++) {
-		// 	printf("%d ", material[i].val);
-		// }
-		// printf("\n");
-		// for (int i = 0; i < N; i++) {
-		// 	printf("%d ", material[i].line);
-		// }
-		// printf("\n");
 	}
 
 	for (int i = 0; i < N; i++) printf("%d ", material[i].val);
-
-	return 0;
 }
 
-long GCD(long a, long b) {
+long GCD(int a, int b) {
 	while (b > 0) {
 		long tmp = a;
 		a = b;
@@ -61,19 +52,17 @@ long GCD(long a, long b) {
 	return a;
 }
 
-// int LCM(int a, int b) {
-// 	return a * b / GCD(a, b);
-// }
+int LCM(int a, int b) {
+	return a * b / GCD(a, b);
+}
 
 void update(int idx, int val, int line, int cnt) {
 	int branch = material[idx].line;
 	material[idx].val *= val;
 	material[idx].line = line;
-	if (cnt == 1 && branch != line && branch != -1) {
+	if (cnt == 1) {
 		for (int i = 0; i < N; i++) {
-			if (material[i].line == branch && i != idx) {
-				update(i, val, line, 2);
-			}
+			if (material[i].line = branch && i != idx) update(i, val, line, 2);
 		}
 	}
 }
