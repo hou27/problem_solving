0e274785301a01800a47823bfbeabff7b1b75ffe
hou27 <ataj125@gmail.com>
2022-06-28 19:55:20 +0900

    5622

---
+++ java/baekjoon/5622/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    sc.close();
+
+    char[] charArr = str.toCharArray();
+
+    int time = 0;
+    for (char c : charArr) {
+      if (c >= 'A' && c <= 'C') {
+        time += 3;
+      } else if (c >= 'D' && c <= 'F') {
+        time += 4;
+      } else if (c >= 'G' && c <= 'I') {
+        time += 5;
+      } else if (c >= 'J' && c <= 'L') {
+        time += 6;
+      } else if (c >= 'M' && c <= 'O') {
+        time += 7;
+      } else if (c >= 'P' && c <= 'S') {
+        time += 8;
+      } else if (c >= 'T' && c <= 'V') {
+        time += 9;
+      } else if (c >= 'W' && c <= 'Z') {
+        time += 10;
+      }
+    }
+    System.out.println(time);
+  }
+}
