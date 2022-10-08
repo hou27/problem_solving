95d1e9fb072be9b8744a945a56f9dc559ee2b48b
hou27 <ataj125@gmail.com>
2022-10-08 22:00:57 +0900

    elice java 개굴개굴 개구리

---
+++ java/elice/개굴개굴_개구리/Main.java
@@ -0,0 +1,20 @@
+package java.elice.개굴개굴_개구리;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        /* 여기에 코드를 작성해 주세요 */
+        Scanner sc = new Scanner(System.in);
+
+        String line = sc.nextLine();
+        char[] chars = line.toCharArray();
+
+        for (int i = 0; i < line.length(); i++) {
+            if (chars[i] == ' ')
+                System.out.print(" ");
+            else
+                System.out.print("개굴");
+        }
+    }
+}
