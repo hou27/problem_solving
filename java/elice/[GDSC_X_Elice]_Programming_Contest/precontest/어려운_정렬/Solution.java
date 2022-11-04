d30ea59c3a6b92472fcda643dc27bd9739eee17d
hou27 <ataj125@gmail.com>
2022-11-04 16:44:22 +0900

    elice 사전대회 테스트

---
+++ java/elice/[GDSC_X_Elice]_Programming_Contest/precontest/어려운_정렬/Solution.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+  // arr : testcase.txt의 입력 데이터가 들어있습니다.
+  // ㄴ ex) [1, 2, 3, 4, 5]
+  public static ArrayList<Integer> solution(ArrayList<Integer> arr) {
+    Collections.sort(arr); // 오름차순 정렬
+
+    return arr;
+  }
+}
