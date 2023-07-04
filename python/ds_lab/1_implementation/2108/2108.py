23bd0a8421855b4965655f47acb596342dc86678
hou27 <ataj125@gmail.com>
2023-07-04 20:08:11 +0900

    ds_lab: baekjoon python 통계학

--- python/ds_lab/1_implementation/2108/2108.py
+++ python/ds_lab/1_implementation/2108/2108.py
@@ -1,8 +1,11 @@
-N = int(input())
+# ver2. sys.stdin.readline() 사용
+import sys
 
+# 입력
+N = int(sys.stdin.readline())
 nums = []
 for _ in range(N):
-    nums.append(int(input()))
+    nums.append(int(sys.stdin.readline()))
 
 # 산술평균
 print(round(sum(nums) / N))
@@ -12,17 +15,23 @@ nums.sort()
 print(nums[N // 2])
 
 # 최빈값
-counter = []
-for num in nums:
-    for i in range(len(counter)):
-        if counter[i][0] == num:
-            counter[i][1] += 1
-            break
-    else:
-        counter.append([num, 1])
 
+# ver1
+# counter = []
+# for num in nums:
+#     for i in range(len(counter)):
+#         if counter[i][0] == num:
+#             counter[i][1] += 1
+#             break
+#     else:
+#         counter.append([num, 1])
+#
 # count[i][1]을 기준으로 정렬
-counter.sort(key=lambda x: x[1], reverse=True)
+# counter.sort(key=lambda x: x[1], reverse=True)
+
+# ver2. Counter를 사용한 버전
+from collections import Counter
+counter = Counter(nums).most_common()
 
 # 최빈값이 여러 개인 경우, 두 번째로 작은 값 출력
 if len(counter) > 1 and counter[0][1] == counter[1][1]:
