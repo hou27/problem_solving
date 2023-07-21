bc19d75dd40c876feb6cd37d075c9f1adabdd2f0
hou27 <ataj125@gmail.com>
2023-07-21 16:40:15 +0900

    ds_lab: baekjoon python 스택 수열

--- python/ds_lab/4_data_structure/1874/1874.py
+++ python/ds_lab/4_data_structure/1874/1874.py
@@ -1,20 +1,24 @@
 n = int(input())
 
-nums = [e for e in range(1, n + 1)]
-goal = [int(input()) for _ in range(n)]
-tmp = []
-ans = []
+# 초기 설정
+nums = 2 # 1은 무조건 들어가니까 2부터 시작
+tmp = [1] # stack
+ans = ['+'] # 결과
 
 for _ in range(n):
-    tmp.append(nums.pop(0))
-    ans.append('+')
-    while tmp and tmp[-1] == goal[0]:
-        tmp.pop()
-        goal.pop(0)
-        ans.append('-')
+    goal = int(input()) # 복잡도를 줄이기 위해 매 반복마다 목표를 입력받음
 
-if tmp:
-    print('NO')
-else:
-    for e in ans:
-        print(e)
+    while nums <= goal: # 목표가 될 때까지
+        tmp.append(nums) # 스택에 추가
+        nums += 1
+        ans.append('+') # 결과에 추가
+    
+    if tmp[-1] == goal: # 목표가 스택의 마지막 값과 같다면
+        tmp.pop() # 스택에서 제거
+        ans.append('-') # 결과에 추가
+    else: # 목표가 스택의 마지막 값과 다르다면
+        print('NO') # 불가능이므로 NO 출력 후 종료
+        exit(0)
+
+for e in ans:
+    print(e)
