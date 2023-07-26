06b5b8237a097031297b8c2a4a38812184a69049
hou27 <ataj125@gmail.com>
2023-07-26 19:12:20 +0900

    ds_lab: baekjoon python 트리 (틀림)
    해결해야하는 반례:
    3 3
    1 1
    2 3
    1 2
    가 No trees.가 아닌 There is one tree.가 출력됨.

---
+++ python/ds_lab/4_data_structure/4803/4803.py
@@ -0,0 +1,45 @@
+while True:
+    n, m = map(int, input().split())  # 정점의 개수와 간선의 개수
+    cnt = 0  # 트리의 개수
+
+    if n == 0 and m == 0:  # 종료 조건
+        break
+
+    graph = [[] for _ in range(n + 1)]  # 그래프
+
+    # 그래프 입력
+    for i in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    visited = [False] * (n + 1)  # 방문 여부
+
+    # 정점이 n개면 간선이 n-1개이고, 사이클이 없는 트리를 카운트
+    # 즉, 탐색하다가 방문했던 정점을 다시 방문하면 사이클이 있는 것
+    is_tree = True
+
+    def dfs(v, prev):
+        visited[v] = True
+        for i in graph[v]:
+            if not visited[i]:
+                dfs(i, v)
+            elif i != prev:
+                global is_tree
+                is_tree = False
+                return
+
+    # 모든 정점에 대해 dfs 수행
+    for i in range(1, n + 1):
+        if not visited[i]:
+            is_tree = True
+            dfs(i, i)
+            if is_tree:
+                cnt += 1
+
+    if cnt == 1:  # 트리가 1개면
+        print("There is one tree.")
+    elif cnt > 1:  # 트리가 1개보다 많으면
+        print(f"A forest of {cnt} trees.")
+    else:  # 트리가 없으면
+        print("No trees.")
