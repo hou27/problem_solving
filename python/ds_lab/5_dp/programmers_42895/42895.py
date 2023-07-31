d53d5efbf55931c1391f692e8dc4176d079e6391
hou27 <ataj125@gmail.com>
2023-07-31 19:59:13 +0900

    ds_lab: programmers N으로 표현
    경우를 계산하는 순서가 꼬여있어 최소인 경우를 출력하지 못하는 케이스가 있었음

--- python/ds_lab/5_dp/programmers_42895/42895.py
+++ python/ds_lab/5_dp/programmers_42895/42895.py
@@ -1,49 +1,94 @@
+# def solution(N, number):
+#     cnt = 0
+#     if N == number:
+#         return 1
+
+#     # 각 1 ~ 9자리의 N을 이어붙인 숫자 저장
+#     possible_nums = [set() for _ in range(9)]
+#     # for i in range(9):
+#     #     possible_nums[i].add(int(str(N) * (i + 1)))
+#     possible_nums[0].add(N)
+
+#     print(possible_nums)
+
+#     while True:
+#         cnt += 1
+#         curr_possible_nums = set()
+#         # tmp에 빈 set이 아닌 원소만 저장
+#         tmp = list(filter(lambda x: x != set(), possible_nums))
+#         for idx, nums in enumerate(tmp):
+#             if cnt + idx + 1 > 8:
+#                 return -1
+#             for num1 in possible_nums[cnt - 1]:
+#                 for num2 in nums:
+#                     curr_possible_nums = get_possible_nums(num1, num2, N)
+#                     if number in curr_possible_nums:
+#                         return cnt + idx + 1
+#                     else:
+#                         possible_nums[cnt + idx] = possible_nums[cnt + idx].union(
+#                             curr_possible_nums
+#                         )
+
+
+# # 주어진 숫자에 가능한 경우를 모두 반환하는 함수
+# def get_possible_nums(num1, num2, N):
+#     tmp = set()
+#     # 정방향
+#     # tmp.add(num1 * 10 + N)  # N을 하나 붙인 경우
+#     tmp.add(num1 + num2)  # 더한 경우
+#     tmp.add(num1 - num2)  # 뺀 경우
+#     tmp.add(num1 * num2)  # 곱한 경우
+#     tmp.add(num1 // num2)  # 나눈 경우
+
+#     # 역방향
+#     tmp.add(num2 + num1)  # 더한 경우
+#     tmp.add(num2 - num1)  # 뺀 경우
+#     tmp.add(num2 * num1)  # 곱한 경우
+
+#     # set에서 0 아닌 정수만 남기고 제거
+#     tmp = set(filter(lambda x: x != 0, tmp))
+
+#     return tmp
+
+
 def solution(N, number):
-    cnt = 0
     if N == number:
         return 1
 
     possible_nums = [set() for _ in range(9)]
-    possible_nums[0].add(N)
 
-    while cnt < 9:
-        cnt += 1
-        curr_possible_nums = set()
-        # tmp에 빈 set이 아닌 원소만 저장
-        tmp = list(filter(lambda x: x != set(), possible_nums))
-        for idx, nums in enumerate(tmp):
-            if cnt + idx + 1 > 8:
-                return -1
-            for num1 in possible_nums[cnt - 1]:
-                for num2 in nums:
-                    curr_possible_nums = get_possible_nums(num1, num2, N)
+    for i in range(1, 9):
+        possible_nums[i].add(int(str(N) * (i)))
+        if number in possible_nums[i]:
+            return i
+        print(possible_nums)
+        for j in range(i // 2 + 1):
+            for num1 in possible_nums[j]:
+                for num2 in possible_nums[i - j]:
+                    curr_possible_nums = get_possible_nums(num1, num2)
                     if number in curr_possible_nums:
-                        return cnt + idx + 1
+                        return i
                     else:
-                        possible_nums[cnt + idx] = possible_nums[cnt + idx].union(
-                            curr_possible_nums
-                        )
+                        possible_nums[i] = possible_nums[i].union(curr_possible_nums)
 
     return -1
 
 
 # 주어진 숫자에 가능한 경우를 모두 반환하는 함수
-def get_possible_nums(num1, num2, N):
+def get_possible_nums(num1, num2):
     tmp = set()
     # 정방향
-    tmp.add(num1 * 10 + N)  # N을 하나 붙인 경우
     tmp.add(num1 + num2)  # 더한 경우
     tmp.add(num1 - num2)  # 뺀 경우
     tmp.add(num1 * num2)  # 곱한 경우
-    tmp.add(num1 // num2)  # 나눈 경우
+    if num2 != 0:
+        tmp.add(num1 // num2)  # 나눈 경우
 
     # 역방향
-    tmp.add(num2 + num1)  # 더한 경우
     tmp.add(num2 - num1)  # 뺀 경우
     tmp.add(num2 * num1)  # 곱한 경우
-
-    # set에서 0 아닌 정수만 남기고 제거
-    tmp = set(filter(lambda x: x != 0, tmp))
+    if num1 != 0:
+        tmp.add(num2 // num1)
 
     return tmp
 
@@ -63,4 +108,7 @@ if __name__ == "__main__":
     print(solution(N, number))
     N = 5
     number = 26
+    print(solution(N, number))
+    N = 4
+    number = 31
     print(solution(N, number))
