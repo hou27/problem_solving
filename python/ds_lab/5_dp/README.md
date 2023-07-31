980f0133303007a1c1dac62b073785889fbe2314
hou27 <ataj125@gmail.com>
2023-07-31 18:36:57 +0900

    ds_lab: programmers N으로 표현 (8번 케이스 실패)

---
+++ python/ds_lab/5_dp/README.md
@@ -0,0 +1,7 @@
+# Dynamic Programming
+
+[N으로 표현](https://www.notion.so/Programmers-N-1a41fe5980914931b8ccc2adae8c77af?pvs=21)
+
+[1010 다리 놓기](https://www.notion.so/1010-0d1e0b1c6d244bc6810f9c5b2523e790?pvs=21)
+
+[9465 스티커](https://www.notion.so/9465-7ad8c42e157b4014a6776de0096a5132?pvs=21)
---
+++ python/ds_lab/5_dp/programmers_42895/42895.py
@@ -0,0 +1,66 @@
+def solution(N, number):
+    cnt = 0
+    if N == number:
+        return 1
+
+    possible_nums = [set() for _ in range(9)]
+    possible_nums[0].add(N)
+
+    while cnt < 9:
+        cnt += 1
+        curr_possible_nums = set()
+        # tmp에 빈 set이 아닌 원소만 저장
+        tmp = list(filter(lambda x: x != set(), possible_nums))
+        for idx, nums in enumerate(tmp):
+            if cnt + idx + 1 > 8:
+                return -1
+            for num1 in possible_nums[cnt - 1]:
+                for num2 in nums:
+                    curr_possible_nums = get_possible_nums(num1, num2, N)
+                    if number in curr_possible_nums:
+                        return cnt + idx + 1
+                    else:
+                        possible_nums[cnt + idx] = possible_nums[cnt + idx].union(
+                            curr_possible_nums
+                        )
+
+    return -1
+
+
+# 주어진 숫자에 가능한 경우를 모두 반환하는 함수
+def get_possible_nums(num1, num2, N):
+    tmp = set()
+    # 정방향
+    tmp.add(num1 * 10 + N)  # N을 하나 붙인 경우
+    tmp.add(num1 + num2)  # 더한 경우
+    tmp.add(num1 - num2)  # 뺀 경우
+    tmp.add(num1 * num2)  # 곱한 경우
+    tmp.add(num1 // num2)  # 나눈 경우
+
+    # 역방향
+    tmp.add(num2 + num1)  # 더한 경우
+    tmp.add(num2 - num1)  # 뺀 경우
+    tmp.add(num2 * num1)  # 곱한 경우
+
+    # set에서 0 아닌 정수만 남기고 제거
+    tmp = set(filter(lambda x: x != 0, tmp))
+
+    return tmp
+
+
+if __name__ == "__main__":
+    N = 5
+    number = 12
+    print(solution(N, number))
+    N = 2
+    number = 11
+    print(solution(N, number))
+    N = 5
+    number = 31168
+    print(solution(N, number))
+    N = 6
+    number = 5
+    print(solution(N, number))
+    N = 5
+    number = 26
+    print(solution(N, number))
