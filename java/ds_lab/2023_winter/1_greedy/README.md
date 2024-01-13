05e99bc07a79814a60721baca85f534bebb67c4d
hou27 <ataj125@gmail.com>
2024-01-13 23:49:02 +0900

    ds_lab: programmers java 단속카메라 (푸는 중)
    아직 풀지 못했지만 너무 더러워진 것 같아 머리 식히기

--- java/ds_lab/2023_winter/1_greedy/README.md
+++ java/ds_lab/2023_winter/1_greedy/README.md
@@ -1,3 +1,7 @@
 # Greedy
 
 [11497 통나무 건너뛰기](./11497/README.md)
+
+[19941 햄버거 분배](./19941/README.md)
+
+[Programmers 단속카메라](./programmers_42884/README.md)
---
+++ java/ds_lab/2023_winter/1_greedy/programmers_42884/solution.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] routes = {{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}};
+        System.out.println(s.solution(routes));
+        int[][] routes2 = {{-2,-1}, {1,2}, {-3,0}};
+        System.out.println(s.solution(routes2));
+        int[][] routes3 = {{0,0}, {0,0}, {2,2}};
+        System.out.println(s.solution(routes3));
+        int[][] routes4 = {{0,1}, {1,2}, {3,4}, {5,6}, {7,8}, {9,10}, {11,12}, {13,14}};
+        System.out.println(s.solution(routes4));
+        int[][] routes5 = {{-20,15},{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}};
+        System.out.println(s.solution(routes5));
+        int[][] routes6 = {{-20,-15}, {-16,-10}, {-18,-13}, {-15,-11}};
+        System.out.println(s.solution(routes6));
+    }
+    public int solution(int[][] routes) {
+        int answer = 0;
+
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[0], b[0]));
+        // System.out.println("After sorting");
+        System.out.println(Arrays.deepToString(routes));
+        
+        int currentSpot = routes[0][1];
+        int prevSpot = -99999;
+        for (int i = 1; i < routes.length; i++) {
+            System.out.println(routes[i][0] + ", " + routes[i][1]);
+            if (currentSpot < routes[i][0]) {
+                if (prevSpot != currentSpot) {
+                    answer++;
+                    prevSpot = currentSpot;
+                    System.out.println("1. currentSpot: " + currentSpot);
+
+                }
+                currentSpot = routes[i][1];
+            }
+            else if(currentSpot <= routes[i][1]) {
+                if (prevSpot == currentSpot) {
+                    continue;
+                }
+                answer++;
+                prevSpot = currentSpot;
+                System.out.println("2. currentSpot: " + currentSpot);
+                currentSpot = routes[i][1];
+
+            }
+            else if(currentSpot > routes[i][1]) {
+                currentSpot = routes[i][1];
+                answer++;
+                System.out.println("3. currentSpot: " + currentSpot);
+                if (i != routes.length - 1)
+                    currentSpot = routes[i+1][0];
+                else
+                    break;
+            }
+            else
+                continue;
+        }
+
+        return answer;
+    }
+}
