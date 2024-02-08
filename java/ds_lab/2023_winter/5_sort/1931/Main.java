471a7a30261b2367211739ce800828449be22d1d
hou27 <ataj125@gmail.com>
2024-02-08 19:39:13 +0900

    ds_lab: baekjoon java 회의실 배정(추가 반례를 통해 해결)
    4
    1 4
    4 4
    4 4
    4 5
    위 반례 덕분에 해결함

--- java/ds_lab/2023_winter/5_sort/1931/Main.java
+++ java/ds_lab/2023_winter/5_sort/1931/Main.java
@@ -1,52 +1,60 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
+
+class Meeting implements Comparable<Meeting> {
+    int start;
+    int end;
+
+    public Meeting(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    // 시작 시간 기준 오름차순 정렬(시작 시간이 같다면 끝나는 시간 기준)
+    @Override
+    public int compareTo(final Meeting o) {
+        if (o.start == start) {
+            return end - o.end;
+        }
+        return start - o.start;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] schedule = new int[N][2];
+        Meeting[] meetings = new Meeting[N];
         int cnt = 1;
         
         for (int i = 0; i < N; i++) {
-            schedule[i][0] = sc.nextInt();
-            schedule[i][1] = sc.nextInt();            
+            meetings[i] = new Meeting(sc.nextInt(), sc.nextInt());       
         }
 
-        // 시작 시간 기준 오름차순 정렬(시작 시간이 같다면 끝나는 시간 기준)
-        Arrays.sort(schedule, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] s1, int[] s2){
-                if(s1[0] == s2[0]) {
-                    return s1[1] - s2[1];
-                }
-                else {
-                    return s1[0] - s2[0];
-                }
-            }
-        });
+        Arrays.sort(meetings);
 
-        int prevEndTime = schedule[0][1];
-        // 시작 시간 같은 것 제외(끝나는 시간이 가장 빠른 것만을 포함)
+        int prevEndTime = meetings[0].end;
         for (int i = 1; i < N; i++) {
-            if (schedule[i][0] == schedule[i - 1][0]) {
-                if (schedule[i][1] == schedule[i - 1][1]) { // 시작시간과 끝시간이 같은 경우 여러 회의 선택 가능..?
+            if (meetings[i].start == meetings[i - 1].start) { // 시작 시간 같은 것 제외(끝나는 시간이 가장 빠른 것만을 포함)
+                if (meetings[i].end == meetings[i - 1].end && meetings[i].start == meetings[i - 1].end) { // 시작시간과 끝시간이 같은 경우 여러 회의 선택 가능..?
+                    cnt++;
+                }
+                else if (meetings[i].start == meetings[i - 1].end) {
                     cnt++;
                 }
                 continue;
             }
-            else if (schedule[i][0] >= prevEndTime) {
+            if (meetings[i].start >= prevEndTime) {
                 cnt++;
-                prevEndTime = schedule[i][1];
+                prevEndTime = meetings[i].end;
             }
-            else if (schedule[i][0] < prevEndTime && schedule[i][1] < prevEndTime) {
-                prevEndTime = schedule[i][1];
+            else if (meetings[i].start < prevEndTime && meetings[i].end < prevEndTime) {
+                prevEndTime = meetings[i].end;
             }
         }
 
         System.out.println(cnt);
 
-        sc.close();        
+        sc.close();
     }
 }
