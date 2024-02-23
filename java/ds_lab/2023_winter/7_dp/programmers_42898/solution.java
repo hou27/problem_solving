35c5fc8c05f7d5c5448d0795868fab2a2b02951b
hou27 <ataj125@gmail.com>
2024-02-23 19:20:14 +0900

    ds_lab: programmers java 등굣길

--- java/ds_lab/2023_winter/7_dp/programmers_42898/solution.java
+++ java/ds_lab/2023_winter/7_dp/programmers_42898/solution.java
@@ -28,10 +28,10 @@ class Solution {
                     continue;
                 }
                 if (dp[i - 1][j] != -1) {
-                    dp[i][j] += dp[i - 1][j];
+                    dp[i][j] += dp[i - 1][j] % 1000000007;
                 }
                 if (dp[i][j - 1] != -1) {
-                    dp[i][j] += dp[i][j - 1];
+                    dp[i][j] += dp[i][j - 1] % 1000000007;
                 }
             }
         }
