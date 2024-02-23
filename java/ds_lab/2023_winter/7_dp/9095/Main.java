6d60219efa9e70b0c4ab768a4aed22880b13d679
hou27 <ataj125@gmail.com>
2024-02-23 19:55:18 +0900

    ds_lab: baekjoon java 1, 2, 3 더하기
    점화식을 찾아내어 품

---
+++ java/ds_lab/2023_winter/7_dp/9095/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt(); // 테스트 케이스 개수
+        int[] dp = new int[11];
+
+        // dp 배열 계산
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        for (int i = 0; i < T; i++) {
+            System.out.println(dp[sc.nextInt()]);
+        }
+
+        sc.close();
+    }
+}
