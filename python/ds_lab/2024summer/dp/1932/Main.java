ed772e0bb5426800627ba9c5b0af8f68817de88a
hou27 <ataj125@gmail.com>
2024-08-22 20:28:50 +0900

    ds_lab: baekjoon java 정수 삼각형

---
+++ python/ds_lab/2024summer/dp/1932/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] triangle = new int[n][n];
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = sc.nextInt();
+            }
+        }
+
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = triangle[n - 1][i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
+            }
+        }
+
+        System.out.println(dp[0][0]);
+    }
+
+}
