594caf78b61bee22589411f1ad1ad9dc4a25acb0
hou27 <ataj125@gmail.com>
2025-01-02 15:25:37 +0900

    leetcode java 542. 0 1 Matrix
    각 0에서 bfs 시작

--- java/leetcode/542/Main.java
+++ java/leetcode/542/Main.java
@@ -11,25 +11,29 @@ public class Main {
 
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int[][] output = new int[mat.length][mat[0].length];
+        Queue<int[]> queue = new LinkedList<>();
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
-                output[i][j] = bfs(mat, i, j);
+                if (mat[i][j] == 1) {
+                    mat[i][j] = -1;
+                } else {
+                    queue.add(new int[] { i, j, 0 });
+                }
             }
         }
+        bfs(mat, queue);
 
-        return output;
+        return mat;
     }
 
-    static int bfs(int[][] mat, int initX, int initY) {
+    static void bfs(int[][] mat, Queue<int[]> queue) {
         int m = mat.length, n = mat[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
         int[] dx = { 0, 0, -1, 1 };
         int[] dy = { -1, 1, 0, 0 };
-        queue.add(new int[] { initX, initY, 0 });
-        int dist = 0;
+        // queue.add(new int[] {initX, initY, 0});
+        // int dist = 0;
 
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
@@ -38,12 +42,13 @@ class Solution {
             if (visited[x][y])
                 continue;
 
-            if (mat[x][y] == 0) {
-                dist = curr[2];
-                break;
-            }
-
             visited[x][y] = true;
+
+            // if (mat[x][y] != 0) {
+            // // dist = curr[2];
+            // mat[x][y] = curr[2];
+            // break;
+            // }
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
@@ -52,12 +57,12 @@ class Solution {
                 if (nx >= m || nx < 0 || ny >= n || ny < 0) {
                     continue;
                 }
-                if (!visited[nx][ny]) {
+                if (!visited[nx][ny] && mat[nx][ny] == -1) {
                     queue.add(new int[] { nx, ny, curr[2] + 1 });
+                    mat[nx][ny] = curr[2] + 1;
                 }
             }
         }
 
-        return dist;
     }
 }
