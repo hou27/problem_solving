15e796861dbf1b6ef38d4d741325ebc0d252ef8d
hou27 <ataj125@gmail.com>
2025-05-04 17:20:52 +0900

    programmers java 미로 탈출 명령어 (시간 초과)

---
+++ java/programmers/150365/Solution.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class PathRecord {
+    public int posX;
+    public int posY;
+    public String history;
+
+    PathRecord(int posX, int posY, String history) {
+        this.posX = posX;
+        this.posY = posY;
+        this.history = history;
+    }
+}
+
+class Solution {
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        int[][] map = new int[n][m];
+
+        List<String> candidates = bfs(map, n, m, x - 1, y - 1, r - 1, c - 1, k);
+
+        if (candidates.size() == 0) {
+            return "impossible";
+        }
+        candidates.sort(null);
+        answer = candidates.get(0);
+
+        return answer;
+    }
+
+    // bfs로 k hop 경로 전부 탐색 -> 마지막 위치가 목적지인 것 중 사전 순으로 가장 빠른 것 리턴
+    static List<String> bfs(int[][] map, int n, int m, int x, int y, int r, int c, int k) {
+        Queue<PathRecord> queue = new LinkedList<>();
+        int[] dx = { 1, -1, 0, 0 };
+        int[] dy = { 0, 0, 1, -1 };
+        char[] direction = { 'd', 'u', 'r', 'l' };
+        List<String> candidates = new ArrayList<>();
+        queue.add(new PathRecord(x, y, ""));
+
+        while (!queue.isEmpty()) {
+            PathRecord curr = queue.poll();
+            int posX = curr.posX;
+            int posY = curr.posY;
+            String history = curr.history;
+
+            if (history.length() == k) {
+                if (posX == r && posY == c) {
+                    candidates.add(history);
+                }
+                continue;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = posX + dx[i];
+                int ny = posY + dy[i];
+                String nHistory = history + direction[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+                queue.add(new PathRecord(nx, ny, nHistory));
+            }
+        }
+
+        return candidates;
+    }
+}
