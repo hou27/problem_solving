8f4ffd0b6e3916df0da85a2d477d8865229507ce
hou27 <ataj125@gmail.com>
2025-06-30 17:39:10 +0900

    programmers java 도둑질

--- java/programmers/42897/Solution.java
+++ java/programmers/42897/Solution.java
@@ -9,20 +9,26 @@ class Solution {
         dp1[0] = money[0];
         dp1[1] = money[0];
 
-        dp(money, dp1, 1);
+        int case1 = dp(money, dp1, 1);
 
         int[] dp2 = new int[n];
         dp2[0] = 0;
         dp2[1] = money[1];
 
-        dp(money, dp2, 0);
+        int case2 = dp(money, dp2, 0);
 
-        return dp1[n - 2] > dp2[n - 2] ? dp1[n - 2] : dp2[n - 2];
+        return case1 > case2 ? case1 : case2;
     }
 
-    static void dp(int[] money, int[] dp, int flag) {
-        for (int i = 2; i < money.length - flag; i++) {
-            dp[i] = dp[i - 1] > dp[i - 2] + money[i] ? dp[i - 1] : dp[i - 2] + money[i];
+    static int dp(int[] money, int[] dp, int flag) {
+        int n = money.length;
+        for (int i = 2; i < n - flag; i++) {
+            dp[i] = money[i] + dp[i - 1] - money[i - 1] > dp[i - 2] + money[i] ? money[i] + dp[i - 1] - money[i - 1]
+                    : dp[i - 2] + money[i];
+            // dp[i] = dp[i] > money[i] + dp[i - 1] - money[i - 1] ? dp[i] : money[i] + dp[i
+            // - 1] - money[i - 1];
         }
+
+        return dp[n - 1 - flag] > dp[n - 2 - flag] ? dp[n - 1 - flag] : dp[n - 2 - flag];
     }
 }
