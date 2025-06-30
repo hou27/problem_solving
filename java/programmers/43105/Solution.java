24ca2a0eebe53960152077aec59fe25de91151bf
hou27 <ataj125@gmail.com>
2025-06-30 17:45:09 +0900

    programmers java 정수 삼각형
    list를 int arr로 바꿔서 해결

--- java/programmers/43105/Solution.java
+++ java/programmers/43105/Solution.java
@@ -1,36 +1,66 @@
 import java.util.*;
 
+// class Solution {
+//     public int solution(int[][] triangle) {
+//         int n = triangle.length;
+//         List<List<Integer>> dp = new ArrayList<>();
+//         List<Integer> firstRow = new ArrayList<>();
+//         firstRow.add(triangle[0][0]);
+//         dp.add(firstRow);
+
+//         for (int i = 1; i < n; i++) {
+//             List<Integer> h = new ArrayList<>();
+//             List<Integer> prevRow = dp.get(i - 1);
+//             for (int j = 0; j < i + 1; j++) {
+//                 int currValue = triangle[i][j];
+//                 int value = -1;
+//                 if (j == 0) {
+//                     value = prevRow.get(j) + currValue;
+//                 } else if (j == i) {
+//                     value = prevRow.get(j - 1) + currValue;
+//                 } else {
+//                     int case1 = prevRow.get(j - 1) + currValue;
+//                     int case2 = prevRow.get(j) + currValue;
+
+//                     value = case1 > case2 ? case1 : case2;
+//                 }
+//                 h.add(value);
+//             }
+//             dp.add(h);
+//         }
+
+//         List<Integer> lastRow = dp.get(n - 1);
+
+//         return Collections.max(lastRow);
+//     }
+// }
+
 class Solution {
     public int solution(int[][] triangle) {
         int n = triangle.length;
-        List<List<Integer>> dp = new ArrayList<>();
-        List<Integer> firstRow = new ArrayList<>();
-        firstRow.add(triangle[0][0]);
-        dp.add(firstRow);
+        int[][] dp = new int[n][n];
+
+        dp[0][0] = triangle[0][0];
 
         for (int i = 1; i < n; i++) {
-            List<Integer> h = new ArrayList<>();
-            List<Integer> prevRow = dp.get(i - 1);
-            for (int j = 0; j < i + 1; j++) {
+            for (int j = 0; j <= i; j++) {
                 int currValue = triangle[i][j];
-                int value = -1;
                 if (j == 0) {
-                    value = prevRow.get(j) + currValue;
+                    dp[i][j] = dp[i - 1][j] + currValue;
                 } else if (j == i) {
-                    value = prevRow.get(j - 1) + currValue;
+                    dp[i][j] = dp[i - 1][j - 1] + currValue;
                 } else {
-                    int case1 = prevRow.get(j - 1) + currValue;
-                    int case2 = prevRow.get(j) + currValue;
-
-                    value = case1 > case2 ? case1 : case2;
+                    dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + currValue;
                 }
-                h.add(value);
             }
-            dp.add(h);
         }
 
-        List<Integer> lastRow = dp.get(n - 1);
+        int max = 0;
+        for (int val : dp[n - 1]) {
+            if (val > max)
+                max = val;
+        }
 
-        return Collections.max(lastRow);
+        return max;
     }
 }
