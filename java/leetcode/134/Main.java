22e306f927046cf73b2544d9ddfaa5258ad77b19
hou27 <ataj125@gmail.com>
2025-07-02 21:50:40 +0900

    leetcode java 134. Gas Station

--- java/leetcode/134/Main.java
+++ java/leetcode/134/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int gl = gas.length;
         int totalGas = 0;
         int totalCost = 0;
         int[] remainGas = new int[gas.length];
@@ -20,13 +21,17 @@ class Solution {
             return -1;
         }
 
-        int maxIndex = 0;
-        for (int i = 1; i < remainGas.length; i++) {
-            if (remainGas[i] > remainGas[maxIndex]) {
-                maxIndex = i;
+        int currGas = 0;
+        int startIdx = 0;
+        for (int i = 0; i < gas.length; i++) {
+            currGas += gas[i] - cost[i];
+
+            if (currGas < 0) {
+                startIdx = i + 1;
+                currGas = 0;
             }
         }
 
-        return maxIndex;
+        return currGas < 0 ? -1 : startIdx;
     }
 }
