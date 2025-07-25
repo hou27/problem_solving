c892c35f1270aa30dba328736839e8bf82bcc270
hou27 <ataj125@gmail.com>
2025-07-25 20:53:31 +0900

    leetcode java 11. Container With Most Water (Solved via two pointer)

--- java/leetcode/11/Main.java
+++ java/leetcode/11/Main.java
@@ -6,17 +6,26 @@ public class Main {
 
 class Solution {
     public int maxArea(int[] height) {
-        int[] dp = new int[height.length];
+        int l = 0, r = height.length - 1;
+        int h, w;
+        int maxArea = -1;
+        int area = 0;
 
-        for (int i = 1; i < height.length; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                int h = height[i] > height[j] ? height[j] : height[i];
-                int w = i - j;
+        while (l <= r) {
+            h = height[l] > height[r] ? height[r] : height[l];
+            w = r - l;
+            area = h * w;
+            if (maxArea < area) {
+                maxArea = area;
+            }
 
-                dp[j] = dp[j] > h * w ? dp[j] : h * w;
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
             }
         }
 
-        return Arrays.stream(dp).max().getAsInt();
+        return maxArea;
     }
 }
