5d45ef790f78557b179b2d6ca3346c4821303b38
hou27 <ataj125@gmail.com>
2026-01-12 18:12:58 +0900

    codetree frequent-problems java. 디지털 로직 패턴 검사
    fix incorrect value type (int to long)

--- java/codetree/frequent-problems/hsat/check-digital-logic-pattern/Main.java
+++ java/codetree/frequent-problems/hsat/check-digital-logic-pattern/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) { // throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String S = sc.next();
         int K = sc.nextInt();
@@ -19,22 +19,20 @@ public class Main {
         map.put(pattern, 1);
 
         // System.out.println(Long.toBinaryString(~(1 << 3)));
+        long mask = ~(1L << K); // 최상위 비트 버리기용
 
         for (int i = K; i < S.length(); i++) {
-            long mask = ~(1 << K); // 최상위 비트 버리기용
             // System.out.println("pattern: " + Long.toBinaryString(pattern));
             // System.out.println("mask: " + Long.toBinaryString(mask));
             pattern = (pattern << 1) & mask | (S.charAt(i) - '0');
-            int cnt = map.getOrDefault(pattern, -1);
+            int cnt = map.getOrDefault(pattern, 0);
             // System.out.println("pattern: " + Long.toBinaryString(pattern) + ", cnt: " +
             // cnt);
             if (cnt == M - 1) {
                 System.out.println(1);
                 return;
-            } else if (cnt != -1) {
-                map.replace(pattern, cnt + 1);
             } else {
-                map.put(pattern, 1);
+                map.put(pattern, cnt + 1);
             }
         }
 
