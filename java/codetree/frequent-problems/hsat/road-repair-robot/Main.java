b217997fb298bcd8097c04bd3d5b4782533cbc71
hou27 <ataj125@gmail.com>
2026-01-19 13:04:22 +0900

    codetree frequent-problems java. 도로 보수 로봇
    binary search로 해결

--- java/codetree/frequent-problems/hsat/road-repair-robot/Main.java
+++ java/codetree/frequent-problems/hsat/road-repair-robot/Main.java
@@ -12,44 +12,52 @@ public class Main {
         for (int i = 0; i < N; i++) {
             positions[i] = sc.nextInt();
         }
-        int initialLength = (int) Math.ceil((double) N / K);
+        // int initialLength = (int) Math.ceil((double) N / K);
 
         sc.close();
         // Please write your code here.
 
+        int fullLength = positions[N - 1] - positions[0] + 1;
         if (K == 1) {
-            System.out.println(positions[N - 1] - positions[0] + 1);
+            System.out.println(fullLength);
             return;
-        } else {
-            for (int i = 0; initialLength + i < positions[N - 1] - positions[0] + 1; i++) {
-                if (check(initialLength + i)) {
-                    System.out.println(initialLength + i);
-                    return;
-                }
-                return;
+        }
+
+        int left = 1;
+        int right = fullLength;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (check(mid)) {
+                right = mid - 1;
+                result = mid;
+            } else {
+                left = mid + 1;
             }
         }
+        System.out.println(result);
     }
 
     private static boolean check(int len) {
-        System.out.println("Length: " + len);
+        // System.out.println("Length: " + len);
         int pos = positions[0];
         int posIdx = 1;
 
         for (int k = 0; k < K; k++) {
-            System.out.println("pos: " + pos);
+            // System.out.println("pos: " + pos);
             pos += len;
 
             while (!(posIdx >= N || pos <= positions[posIdx])) {
                 posIdx++;
-                System.out.println("pos: " + pos + ", posIdx: " + posIdx);
+                // System.out.println("pos: " + pos + ", posIdx: " + posIdx);
             }
             if (posIdx == N) {
                 return true;
             }
             pos = positions[posIdx];
             posIdx++;
-            System.out.println("OUTER:: pos: " + pos + ", posIdx: " + posIdx);
+            // System.out.println("OUTER:: pos: " + pos + ", posIdx: " + posIdx);
         }
 
         if (posIdx > N) {
