573686e2521bdb2ee72fcee78aa0d53f6b033e90
hou27 <ataj125@gmail.com>
2026-01-23 20:14:57 +0900

    codetree frequent-problems java. 채광 로봇 시뮬레이션
    잘못 생각했던 부분을 수정(되돌아가게 만든 경로와 계산하는 점수가 말이 안되는 케이스가 존재했음

--- java/codetree/frequent-problems/hsat/mining-robot-simulation/Main.java
+++ java/codetree/frequent-problems/hsat/mining-robot-simulation/Main.java
@@ -5,6 +5,7 @@ public class Main {
     static int[][] grid;
     static int[][] dp1;
     static int[][] dp2;
+    static final int NEG = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -29,7 +30,7 @@ public class Main {
                     dp1[i][j] = grid[i][j];
                     continue;
                 }
-                int value = Integer.MIN_VALUE;
+                int value = NEG;
                 if (i > 0) {
                     value = Math.max(value, dp1[i - 1][j]);
                 }
@@ -47,7 +48,7 @@ public class Main {
                     dp2[i][j] = grid[i][j];
                     continue;
                 }
-                int value = Integer.MIN_VALUE;
+                int value = NEG;
                 if (i < n - 1) {
                     value = Math.max(value, dp2[i + 1][j]);
                 }
@@ -59,20 +60,72 @@ public class Main {
             }
         }
 
-        int result = Integer.MIN_VALUE;
+        int result = dp1[n - 1][n - 1];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int max = dp1[n - 1][n - 1];
-                for (int cnt = 0; cnt <= t; cnt++) {
-                    if ((i - t + cnt) >= 0 && (j - cnt) >= 0) {
-                        int tmp = dp2[i - t + cnt][j - cnt];
-                        max = max > dp1[i][j] + tmp ? max : dp1[i][j] + tmp;
+                if ((n - 1 - i) + (n - 1 - j) < t) {
+                    continue;
+                }
+
+                int[][] tmp = new int[t + 1][t + 1];
+                for (int a = 0; a <= t; a++) {
+                    Arrays.fill(tmp[a], NEG);
+                }
+                tmp[0][0] = grid[i][j];
+
+                for (int dr = 0; dr <= t; dr++) {
+                    for (int dc = 0; dc <= t; dc++) {
+                        if (dr == 0 && dc == 0) {
+                            continue;
+                        }
+                        if (dr + dc > t) {
+                            continue;
+                        }
+                        int nr = i + dr;
+                        int nc = j + dc;
+                        if (nr >= n || nc >= n) {
+                            continue;
+                        }
+                        int value = NEG;
+                        if (dr > 0) {
+                            value = Math.max(value, tmp[dr - 1][dc]);
+                        }
+                        if (dc > 0) {
+                            value = Math.max(value, tmp[dr][dc - 1]);
+                        }
+                        if (value == NEG) {
+                            continue;
+                        }
+                        tmp[dr][dc] = value + grid[nr][nc];
                     }
                 }
-                result = result > max ? result : max;
+
+                int otherBranch = NEG;
+                for (int dr = 0; dr <= t; dr++) {
+                    int dc = t - dr;
+                    int nr = i + dr;
+                    int nc = j + dc;
+                    if (nr >= n || nc >= n) {
+                        continue;
+                    }
+                    otherBranch = Math.max(otherBranch, tmp[dr][dc]);
+                }
+                if (otherBranch == NEG) {
+                    continue;
+                }
+                int currVal = dp1[i][j] + dp2[i][j] + otherBranch - grid[i][j];
+                result = Math.max(result, currVal);
+
+                // for (int cnt = 0; cnt <= t; cnt++) {
+                // if ((i - t + cnt) >= 0 && (j - cnt) >= 0) {
+                // int tmp = dp2[i - t + cnt][j - cnt];
+                // max = max > dp1[i][j] + tmp ? max : dp1[i][j] + tmp;
+                // }
+                // }
             }
         }
 
         System.out.println(result);
     }
+
 }
