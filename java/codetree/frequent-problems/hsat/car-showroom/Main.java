d85a8b7fad80e4f31ebcecee78b13fc7bce400ea
hou27 <ataj125@gmail.com>
2026-02-06 17:08:42 +0900

    codetree frequent-problems java. 자동차 전시장

--- java/codetree/frequent-problems/hsat/car-showroom/Main.java
+++ java/codetree/frequent-problems/hsat/car-showroom/Main.java
@@ -13,7 +13,7 @@ public class Main {
         k = sc.nextInt();
 
         mem = new int[n + 1][k];
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n + 1; i++) {
             for (int p = 0; p < k; p++) {
                 mem[i][p] = 100001;
             }
@@ -33,6 +33,7 @@ public class Main {
         for (int i = 0; i < k; i++) {
             startPoints[i] = sc.nextInt();
             mem[startPoints[i]][i] = 0;
+            // System.out.println("mem[" + startPoints[i] + "][" + i + "]");
             bfs(startPoints[i], i);
         }
 
@@ -70,14 +71,21 @@ public class Main {
     }
 
     private static int getAnswer() {
-        int min = Integer.MAX_VALUE;
+        int min = 100001;
 
         for (int i = 1; i < n + 1; i++) {
             int currMax = getMaxElement(mem[i], k);
+            if (currMax == -1) {
+                continue;
+            }
 
             if (min > currMax) {
                 min = currMax;
             }
+        }
+
+        if (min == 100001) {
+            return -1;
         }
 
         return min;
@@ -86,6 +94,9 @@ public class Main {
     private static int getMaxElement(int[] arr, int size) {
         int value = Integer.MIN_VALUE;
         for (int i = 0; i < size; i++) {
+            if (arr[i] == 100001) {
+                return -1;
+            }
             if (value < arr[i]) {
                 value = arr[i];
             }
