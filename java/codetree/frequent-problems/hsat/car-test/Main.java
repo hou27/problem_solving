5e268e8fd13b9029cd0a3ce9aa28bfd6ca65a762
hou27 <ataj125@gmail.com>
2026-02-11 17:58:36 +0900

    codetree frequent-problems java. 자동차 테스트
    using HashMap

--- java/codetree/frequent-problems/hsat/car-test/Main.java
+++ java/codetree/frequent-problems/hsat/car-test/Main.java
@@ -6,36 +6,46 @@ public class Main {
         int n = sc.nextInt();
         int q = sc.nextInt();
         int[] a = new int[n];
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
+        }
         int[] queries = new int[q];
-        for (int i = 0; i < q; i++)
+        for (int i = 0; i < q; i++) {
             queries[i] = sc.nextInt();
+        }
         // Please write your code here.
 
         sc.close();
 
         Arrays.sort(a);
 
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(a[i], i);
+        }
+
         for (int i = 0; i < q; i++) {
-            int left = 0;
-            int right = n - 1;
-            int mid = -1;
-            while (left <= right) {
-                mid = (left + right) / 2;
-                if (queries[i] < a[mid]) {
-                    right = mid - 1;
-                } else if (queries[i] > a[mid]) {
-                    left = mid + 1;
-                } else {
-                    break;
-                }
-            }
-            if (a[mid] == queries[i]) {
-                System.out.println((mid + 1 - 1) * (n - (mid + 1)));
-            } else {
+            if (!indexMap.containsKey(queries[i])) {
                 System.out.println(0);
+                continue;
             }
+
+            int idx = indexMap.get(queries[i]);
+            // int left = 0;
+            // int right = n - 1;
+            // int mid = -1;
+            // while (left <= right) {
+            // mid = (left + right) / 2;
+            // if (queries[i] < a[mid]) {
+            // right = mid - 1;
+            // } else if (queries[i] > a[mid]) {
+            // left = mid + 1;
+            // } else {
+            // break;
+            // }
+            // }
+
+            System.out.println((idx + 1 - 1) * (n - (idx + 1)));
         }
     }
 }
