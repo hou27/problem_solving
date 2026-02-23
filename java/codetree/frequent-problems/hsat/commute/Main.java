8c1b912e3789a2d0e608754a97090360eda4e359
hou27 <ataj125@gmail.com>
2026-02-23 21:52:08 +0900

    codetree frequent-problems java. 출퇴근길
    출발지점은 재방문 가능하다는 점을 놓쳤었음

--- java/codetree/frequent-problems/hsat/commute/Main.java
+++ java/codetree/frequent-problems/hsat/commute/Main.java
@@ -27,29 +27,29 @@ public class Main {
         // Please write your code here.
 
         // 가는 길에 방문 가능한 모든 노드 적립
-        boolean[] sToMid = getReachableSet(S, T, edges);
-        boolean[] midToT = getReachableSet(T, S, reverseEdges);
-        for (int i = 0; i < n + 1; i++) {
-            System.out.print(i + ": " + sToMid[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < n + 1; i++) {
-            System.out.print(i + ": " + midToT[i] + " ");
-        }
-        System.out.println();
-        System.out.println();
+        boolean[] sToMid = getReachableSet(S, T, edges, S);
+        boolean[] midToT = getReachableSet(T, S, reverseEdges, S);
+        // for (int i = 0; i < n + 1; i++) {
+        // System.out.print(i + ": " + sToMid[i] + " ");
+        // }
+        // System.out.println();
+        // for (int i = 0; i < n + 1; i++) {
+        // System.out.print(i + ": " + midToT[i] + " ");
+        // }
+        // System.out.println();
+        // System.out.println();
 
         // 오는 길에 방문 가능한 모든 노드 적립
-        boolean[] tToMid = getReachableSet(T, S, edges);
-        boolean[] midToS = getReachableSet(S, T, reverseEdges);
-        for (int i = 0; i < n + 1; i++) {
-            System.out.print(i + ": " + tToMid[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < n + 1; i++) {
-            System.out.print(i + ": " + midToS[i] + " ");
-        }
-        System.out.println();
+        boolean[] tToMid = getReachableSet(T, S, edges, T);
+        boolean[] midToS = getReachableSet(S, T, reverseEdges, T);
+        // for (int i = 0; i < n + 1; i++) {
+        // System.out.print(i + ": " + tToMid[i] + " ");
+        // }
+        // System.out.println();
+        // for (int i = 0; i < n + 1; i++) {
+        // System.out.print(i + ": " + midToS[i] + " ");
+        // }
+        // System.out.println();
 
         // 둘의 교집합 크기 반환
         int cnt = 0;
@@ -65,17 +65,24 @@ public class Main {
         System.out.println(cnt);
     }
 
-    private static boolean[] getReachableSet(int startVertex, int endVertex, List<List<Integer>> edges) {
+    private static boolean[] getReachableSet(int startVertex, int endVertex, List<List<Integer>> edges,
+            int realStartVertex) {
         boolean[] visited = new boolean[n + 1];
         Deque<Integer> queue = new ArrayDeque<>();
         queue.add(startVertex);
         visited[startVertex] = true;
+        int realStartVertexCnt = 0;
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
 
             if (cur == endVertex) {
-                continue;
+                // 출발 지점은 다시 방문 가능
+                if (endVertex == realStartVertex && realStartVertexCnt == 0) {
+                    realStartVertexCnt++;
+                } else {
+                    continue;
+                }
             }
 
             for (int next : edges.get(cur)) {
